bb9081af27e3fbe9fc5fb4bc9ff894aa43024d96
Artur Trifonov <wrgraff@gmail.com>
2020-11-07 15:37:48 +0300

    added circle

--- index.js
+++ index.js
@@ -1,41 +1,22 @@
-class Timer {
-    constructor(durationInput, startButton, pauseButton) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
-
-        this.startButton.addEventListener('click', this.start);
-        this.pauseButton.addEventListener('click', this.pause);
-    };
-
-    start = () => {
-        this.tick();
-        this.timerId = setInterval(this.tick, 1000);
-    };
-
-    pause = () => {
-        clearInterval(this.timerId);
-    };
-
-    tick = () => {
-        if (this.timeRemaining <= 0) {
-            this.pause();
-        } else {
-            this.timeRemaining = this.timeRemaining - 1;
-        };
-    };
-
-    get timeRemaining() {
-        return parseFloat(this.durationInput.value);
-    };
-
-    set timeRemaining(time) {
-        this.durationInput.value = time;
-    };
-};
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let duration;
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart(totalDuration) {
+        duration = totalDuration;
+    },
+    onTick(timeRemaining) {
+        circle.setAttribute('stroke-dashoffset',
+            perimeter * timeRemaining / duration - perimeter
+        );
+    },
+    onComplete() {
+        console.log('timer is dead')
+    }
+});
